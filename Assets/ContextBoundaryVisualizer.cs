ba949a02da1d2c8cbaad3033c8cfacf1221a037c
kevroy314 <kevin.horecka@gmail.com>
2017-09-29 23:50:53 -0500

    Finished CB Metric Visualization

--- Assets/ContextBoundaryVisualizer.cs
+++ Assets/ContextBoundaryVisualizer.cs
@@ -22,8 +22,14 @@ public class ContextBoundaryVisualizer : MonoBehaviour {
 
     private float leftScale;
     private float rightScale;
-    private float leftOffset;
-    private float rightOffset;
+
+    private Hashtable leftItemAnimationHash;
+    private Hashtable middleItemAnimationHash;
+    private Hashtable rightItemAnimationHash;
+
+    public float animationTime;
+    public iTween.LoopType loopType;
+    public iTween.EaseType easeType;
 
     // Use this for initialization
     void Start () {
@@ -38,25 +44,59 @@ public class ContextBoundaryVisualizer : MonoBehaviour {
         leftScale = 1f / (middleItemTransform.anchoredPosition3D.x - leftItemTransform.anchoredPosition3D.x);
         rightScale = 1f / (rightItemTransform.anchoredPosition3D.x - middleItemTransform.anchoredPosition3D.x);
 
-        leftOffset = leftItemTransform.anchoredPosition3D.x;
-        rightOffset = middleItemTransform.anchoredPosition3D.x;
+        leftItemAnimationHash = GetMovementHash("SetLeftLocation", -440f, -300f);
+        middleItemAnimationHash = GetMovementHash("SetMiddleLocation", -30f, -170f);
+        rightItemAnimationHash = GetMovementHash("SetRightLocation", 100f, 240f);
 
-        // TODO: Add looped iTween animation of squish and expand
+        iTween.ValueTo(gameObject, leftItemAnimationHash);
+        iTween.ValueTo(gameObject, middleItemAnimationHash);
+        iTween.ValueTo(gameObject, rightItemAnimationHash);
     }
 	
-	// Update is called once per frame
-	void Update () {
+    Hashtable GetMovementHash(string callbackFunctionName, float from, float to)
+    {
+        Hashtable tmp = new Hashtable();
+        tmp.Add("from", from);
+        tmp.Add("to", to);
+        tmp.Add("time", animationTime);
+        tmp.Add("looptype", loopType);
+        tmp.Add("easetype", easeType);
+        tmp.Add("onupdate", callbackFunctionName);
+        return tmp;
+    }
+
+    void SetLocation(RectTransform trans, float l)
+    {
+        trans.anchoredPosition3D = new Vector3(l, trans.anchoredPosition3D.y, trans.anchoredPosition3D.z);
+    }
+
+    void SetLeftLocation(float l)
+    {
+        SetLocation(leftItemTransform, l);
+    }
+
+    void SetMiddleLocation(float l)
+    {
+        SetLocation(middleItemTransform, l);
+    }
+
+    void SetRightLocation(float l)
+    {
+        SetLocation(rightItemTransform, l);
+    }
+
+    // Update is called once per frame
+    void Update () {
         leftLabelTransform.anchoredPosition3D = new Vector3((leftItemTransform.anchoredPosition3D.x + middleItemTransform.anchoredPosition3D.x) / 2f, leftLabelTransform.anchoredPosition3D.y, leftLabelTransform.anchoredPosition3D.z);
         rightLabelTransform.anchoredPosition3D = new Vector3((rightItemTransform.anchoredPosition3D.x + middleItemTransform.anchoredPosition3D.x) / 2f, rightLabelTransform.anchoredPosition3D.y, rightLabelTransform.anchoredPosition3D.z);
         contextBoundaryLabelTransform.anchoredPosition3D = new Vector3((leftLabelTransform.anchoredPosition3D.x + rightLabelTransform.anchoredPosition3D.x) / 2f, contextBoundaryLabelTransform.anchoredPosition3D.y, contextBoundaryLabelTransform.anchoredPosition3D.z);
 
-        // TODO: Fix this
-        float leftNormDist = (middleItemTransform.anchoredPosition3D.x - leftItemTransform.anchoredPosition3D.x - leftOffset) * leftScale;
-        float rightNormDist = (rightItemTransform.anchoredPosition3D.x - middleItemTransform.anchoredPosition3D.x - rightOffset) * rightScale;
+        float leftNormDist = (middleItemTransform.anchoredPosition3D.x - leftItemTransform.anchoredPosition3D.x) * leftScale - 1;
+        float rightNormDist = (rightItemTransform.anchoredPosition3D.x - middleItemTransform.anchoredPosition3D.x) * rightScale - 1;
 
         leftLabelText.text = leftNormDist.ToString("0.000");
         rightLabelText.text = rightNormDist.ToString("0.000");
 
-        contextBoundaryLabelText.text = (1 - rightNormDist - leftNormDist).ToString("0.000");
+        contextBoundaryLabelText.text = (rightNormDist - leftNormDist).ToString("0.000");
     }
 }
